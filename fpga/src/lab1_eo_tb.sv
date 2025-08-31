329ff2c6fd0636eb848032ae45913b8c1135eb9c
eoinoconnell04 <eoinoconnell04@gmail.com>
2025-08-31 15:45:16 -0700

    Fixed display testbench and testvectors, added top level testbench and vectors

--- fpga/src/lab1_eo_tb.sv
+++ fpga/src/lab1_eo_tb.sv
@@ -11,13 +11,14 @@ Note: Testbench modified from my testbench from E85 Lab 3.
 module display_tb();
     logic clk, reset;
     logic [3:0] s;
+    logic [2:0] led;
     logic [6:0] seg;
-    logic [6:0] expected;
+    logic [8:0] expected;
     logic [31:0] vectornum, errors;
-    logic [10:0] testvectors[10000:0];  // 4 input bits + 7 expected output bits
+    logic [12:0] testvectors[10000:0];  // 4 input bits + 7 expected output bits
 
     // instantiate device under test
-    display dut(s, seg);
+    lab1_eo dut(s, seg, led);
 
     // generate clock
     always begin
@@ -40,13 +41,13 @@ module display_tb();
     // check results on falling edge of clk
     always @(negedge clk)
         if (~reset) begin // skip during reset
-            if (seg !== expected) begin // check result
+            if ({seg, led[1:0]} !== expected) begin // check result
                 $display("Error: input = %b", s);
-                $display(" output = %b (%b expected)", seg, expected);
+                $display(" output = seg: %b, s:%b (%b expected)", seg, s[1:0], expected);
                 errors = errors + 1;
             end
             vectornum = vectornum + 1;
-            if (testvectors[vectornum] === 11'bx) begin
+            if (testvectors[vectornum] === 14'bx) begin
                 $display("%d tests completed with %d errors", vectornum, errors);
                 $stop;
             end
